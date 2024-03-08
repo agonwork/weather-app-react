64468659611842c7d283ad1b99fd0c3939fcae44
agon2002 <ac52981@ubt-uni.net>
2024-03-08 21:28:46 +0100

    Added error show when an invalid location is typed.

--- data.js
+++ data.js
@@ -1,30 +1,33 @@
-import clearImg from './assests/clear.png';
-import cloudImg from './assests/cloud.png';
-import mistImg from './assests/mist.png';
-import rainImg from './assests/rain.png';
-import snowImg from './assests/snow.png';
+import clearImg from './src/assets/clear.png';
+import cloudImg from './src/assets/cloud.png';
+import mistImg from './src/assets/mist.png';
+import rainImg from './src/assets/rain.png';
+import snowImg from './src/assets/snow.png';
 
 //This file is used to get the images for the current weather info based on the json.main[0] object in the API
 
 export const CURRENT_WEATHER = [
     {
-        case :'Clear',
+        weatherDesc :'Clear',
         image: clearImg
     },
     {
-        case :'Clouds',
+        weatherDesc :'Clouds',
         image:cloudImg
     },
     {
-        case :'Haze',
+        weatherDesc :'Haze',
         image:mistImg
     },
     {
-        case :'Rain',
+        weatherDesc :'Rain',
         image:rainImg
     },
     {
-        case :'Snow',
+        weatherDesc :'Snow',
         image:snowImg
+    },
+    {
+        
     }
 ]
--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,95 +1,117 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Search from "../search/Search";
 import { FaLocationDot } from "react-icons/fa6";
-// import { CURRENT_WEATHER } from '../../../data.js';
-
+import { CURRENT_WEATHER } from "../../../data";
 
 //The search component is implemented in this file Weather.jsx
+export function CurrentWeather({ weatherDesc, getWeatherImage }) {
+  const weatherImage = getWeatherImage(weatherDesc); // Call the function to get the image path
 
-
-export default function Weather() {
-    const [search, setSearch] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [weatherData, setWeatherData] = useState(null);
-
-
-    //json data is fetched and it triggers a loading phase 
-    async function fetchWeatherData(param) {
-        setLoading(true)
-        try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&units=metric&appid=972544a3cdc4f40fadbb9bddfefb9117`);
-
-            const data = await response.json();
-            if (data) {
-                setWeatherData(data);
-                setLoading(false);
-            }
-        }
-        catch (e) {
-            setLoading(false);
-            console.log(e);
-
-        }
-    }
-
-    function handleSearch() {
-        fetchWeatherData(search)
-    }
-
-    function getCurrentDate() {
-        return new Date().toLocaleDateString('en-us', {
-            weekday: 'long',
-            month: 'long',
-            day: 'numeric'
-        });
-    }
-
-    useEffect(() => {
-        fetchWeatherData("Prishtina");
-    }, []);
-    console.log(weatherData);
-
-
-    return <div>
-        <Search
-            search={search}
-            setSearch={setSearch}
-            handleSearch={handleSearch}
-        />
-        {
-
-            loading ? <div>Loading...</div>
-                : (
-                    <div className='city-name'>
-                        <h2>
-                            <FaLocationDot />{weatherData?.name}, <span>{weatherData?.sys.country}</span>
-                        </h2>
-
-
-                        <div className="date">
-                            <span>{getCurrentDate}</span>
-                        </div>
-
-
-                        <div className='temp'>{Math.round(weatherData?.main?.temp) + "°C"}</div>
-                        <p className='description'>
-                            {weatherData && weatherData.weather && weatherData.weather[0] ? weatherData.weather[0].description : ''}
-                        </p>
-
-
-                        <div className='weather-info'>
-                            <div>
-                                <p>Wind Speed</p>
-                                <p className='wind'>{weatherData?.wind?.speed} km/h</p>
-                            </div>
-
-                            <div>
-                                <p>Humidity</p>
-                                <p className='humidity'>{weatherData?.main?.humidity}%</p>
-                            </div>
-                        </div>
-                    </div>
-
-                )}
+  return (
+    <div>
+      {weatherImage && (
+        <img className="imageDesc" src={weatherImage} alt={weatherDesc} />
+      )}
     </div>
+  );
+}
+export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  //json data is fetched and it triggers a loading phase
+  async function fetchWeatherData(param) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&units=metric&appid=972544a3cdc4f40fadbb9bddfefb9117`
+      );
+
+      const data = await response.json();
+      if (data.cod && data.cod !== 200) {
+        setWeatherData(null);
+        setLoading(false);
+        
+      } else {
+        setWeatherData(data);
+      }
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
+    }
+  }
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
+
+  useEffect(() => {
+    fetchWeatherData("Prishtina");
+  }, []);
+  console.log(weatherData);
+
+  const getWeatherImage = (weatherDesc) => {
+    const weatherInfo = CURRENT_WEATHER.find(
+      (weather) => weather.weatherDesc === weatherDesc
+    );
+    return weatherInfo ? weatherInfo.image : null;
+  };
+  return (
+    <div>
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="city-name">
+          <h2>
+            <FaLocationDot />
+            {weatherData?.name}, <span>{weatherData?.sys.country}</span>
+          </h2>
+
+          <CurrentWeather
+            weatherDesc={weatherData?.weather[0]?.main}
+            getWeatherImage={getWeatherImage}
+          />
+          <div className="city-name">
+            {weatherData ? (
+              <CurrentWeather
+                weatherMain={weatherData.weather[0].main}
+                getWeatherImage={getWeatherImage}
+              />
+            ) : (
+              <div>
+                <img src="src/assets/404.png" alt="" />
+                <p>Invalid location!</p>
+              </div>
+            )}
+          </div>
+          <div className="temp">
+            {Math.round(weatherData?.main?.temp) + "°C"}
+          </div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+                ? weatherData.weather[0].description
+              : ""}
+          </p>
+
+          <div className="weather-info">
+            <div>
+              <p>Wind Speed</p>
+              <p className="wind">{weatherData?.wind?.speed} km/h</p>
+            </div>
+
+            <div>
+              <p>Humidity</p>
+              <p className="humidity">{weatherData?.main?.humidity}%</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
